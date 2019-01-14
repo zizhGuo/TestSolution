39e27d5885b93e480f4f9e281096091a202f6e89
DESKTOP-NSHSIED\willi <zg2808@rit.edu>
2019-01-14 02:55:13 -0500

    Initialized commit to push current existing project to the new remote repo

---
+++ TestProject1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/***
+ * 
+ ***/
+
+namespace TestProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test(10);
+            test.Print();
+            Console.WriteLine();
+        }
+    }
+    class Test {
+        int tom;
+        public Test(int a) {
+            tom = a;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Tom = " + tom);
+        }
+
+    }
+}
